03fdb6ae4b02b38a3f0fe212ff2657360165ef7c
Jorge E. <jkgonzalezg@hotmail.com>
2024-07-15 09:33:59 -0500

    cambio mensaje de conexión a la base de datos

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const ConectarBD = () => {
     mongoose
         .connect(process.env.MONGO_URL)
-        .then(() => console.log('Estamos conectados con MongoDB'))
+        .then(() => console.log('Estamos conectados a la base de datos de MongoDB'))
         .catch((err) => console.error(err))
 }
 
